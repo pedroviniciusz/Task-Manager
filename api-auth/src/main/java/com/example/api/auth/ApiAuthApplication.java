d85490ce596046c9e4f6b3d06cf68a0ce6f31c02
pedroviniciusz <pedrovdjesus@hotmail.com>
2024-01-22 11:15:24 -0300

    Api de autenticação

--- api-auth/src/main/java/com/example/api/auth/ApiAuthApplication.java
+++ api-auth/src/main/java/com/example/api/auth/ApiAuthApplication.java
@@ -1,13 +1,13 @@
-package com.example.gateway;
+package com.example.api.auth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ApiGatewayApplication {
+public class ApiAuthApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ApiGatewayApplication.class, args);
+		SpringApplication.run(ApiAuthApplication.class, args);
 	}
 
 }
